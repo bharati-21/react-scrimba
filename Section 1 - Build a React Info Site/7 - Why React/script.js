84666416d36a73300666b61966e109ff0bcc4ebd
Bharati Subramanian <bharatisharada@gmail.com>
2021-11-17 12:52:05 +0530

    feat: react because it's declarative

---
+++ Section 1 - Build a React Info Site/7 - Why React/script.js
@@ -0,0 +1,19 @@
+ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// React figures out how to add this line of code to the root.
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+// const h1 = document.createElement('h1');
+// const root = document.querySelector('#root');
+// const textContent = document.createTextNode("I'm learning React!");
+// h1.classList.add('header');
+// h1.appendChild(textContent);
+// root.appendChild(h1);
