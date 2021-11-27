842bbbf6cef94b394df0d8536c78e5943c7b1f36
Bharati Subramanian <bharatisharada@gmail.com>
2021-11-27 13:26:35 +0530

    feat: passing object as props

---
+++ Section 2 - Build an Airbnb Experiences Clone/23-project-pass-object-prop/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from './data'
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
+   
+    const cardData = data.map(item => {
+        return <Card 
+            key={item.id}
+            item={item}
+        />
+    })
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">{cardData}</section>
+        </div>
+    )
+}
